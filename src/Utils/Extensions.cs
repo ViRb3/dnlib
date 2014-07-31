b6a7b8f47c4d4ccbba6a9cc917ac67b34e2d6867
ViR Dash <ViRb3@users.noreply.github.com>
2014-07-31 22:42:18 +0300

    Added extensions.
    Added "GetExtendedName" and "GetAccessorMethods" extensions.

--- src/Utils/Extensions.cs
+++ src/Utils/Extensions.cs
@@ -21,10 +21,78 @@
     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-﻿namespace dnlib.Utils {
+using System.Collections.Generic;
+using dnlib.DotNet;
+
+namespace dnlib.Utils {
 	/// <summary>
 	/// Extension methods
 	/// </summary>
 	public static partial class Extensions {
+        /// <summary>
+        /// Returns the extended name of the type signature so to show full arguments
+        /// </summary>
+        /// <param name="typeSig">Type signature to process</param>
+        /// <returns>The extended name</returns>
+        public static string GetExtendedName(this TypeSig typeSig)
+        {
+            string name = typeSig.TypeName;
+
+            if (typeSig.ToGenericInstSig() != null)
+            {
+                name += "<";
+
+                IList<TypeSig> args = typeSig.ToGenericInstSig().GenericArguments;
+
+                for (int i = 0; i < args.Count; i++)
+                {
+                    string newArgs = args[i].GetExtendedName();
+
+                    if (newArgs != string.Empty)
+                        name += newArgs;
+                    else
+                        name += args[i].TypeName;
+
+                    if (i < args.Count - 1)
+                        name += ", ";
+                }
+
+                name += ">";
+            }
+
+            return name;
+        }
+
+        /// <summary>
+        /// Creates a list with all accessors of a type (Property methods, Event methods ...)
+        /// </summary>
+        /// <param name="type">Type to process</param>
+        /// <returns>List with all accessors</returns>
+        public static List<MethodDef> GetAccessorMethods(this TypeDef type)
+        {
+            var accessorMethods = new List<MethodDef>();
+            foreach (PropertyDef property in type.Properties)
+            {
+                accessorMethods.Add(property.GetMethod);
+                accessorMethods.Add(property.SetMethod);
+                if (property.HasOtherMethods)
+                {
+                    foreach (MethodDef m in property.OtherMethods)
+                        accessorMethods.Add(m);
+                }
+            }
+            foreach (EventDef ev in type.Events)
+            {
+                accessorMethods.Add(ev.AddMethod);
+                accessorMethods.Add(ev.RemoveMethod);
+                accessorMethods.Add(ev.InvokeMethod);
+                if (ev.HasOtherMethods)
+                {
+                    foreach (MethodDef m in ev.OtherMethods)
+                        accessorMethods.Add(m);
+                }
+            }
+            return accessorMethods;
+        }
 	}
 }
