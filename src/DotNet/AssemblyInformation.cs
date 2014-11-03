ec02e38db6a9601405c9d8a2308641b95231f2a4
ViR Dash <ViRb3@users.noreply.github.com>
2014-11-03 19:24:11 +0200

    Added missing AssemblyInformation class documentation

--- src/DotNet/AssemblyInformation.cs
+++ src/DotNet/AssemblyInformation.cs
@@ -9,14 +9,41 @@ namespace dnlib.DotNet
     /// </summary>
     public class AssemblyInformation
     {
+        /// <summary>
+        /// Assembly Company
+        /// </summary>
         public string AssemblyCompany;
+        /// <summary>
+        /// Assembly Configuration
+        /// </summary>
         public string AssemblyConfiguration;
+        /// <summary>
+        /// Assembly Copyright
+        /// </summary>
         public string AssemblyCopyright;
+        /// <summary>
+        /// Assembly Description
+        /// </summary>
         public string AssemblyDescription;
+        /// <summary>
+        /// Assembly File Version
+        /// </summary>
         public string AssemblyFileVersion;
+        /// <summary>
+        /// Assembly Product
+        /// </summary>
         public string AssemblyProduct;
+        /// <summary>
+        /// Assembly Title
+        /// </summary>
         public string AssemblyTitle;
+        /// <summary>
+        /// Assembly Trademark
+        /// </summary>
         public string AssemblyTrademark;
+        /// <summary>
+        /// Assembly Version
+        /// </summary>
         public string AssemblyVersion;
     }
 }
